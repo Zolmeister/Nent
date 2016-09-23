41fe38f28863cd720259506ae7b628f6bb9efb6f
Zolmeister <zolikahan@gmail.com>
2016-09-23 13:39:44 -0500

    dockerize

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ app.get('/', function(req, res) {
   res.sendfile('public/index.html')
 });
 
+app.get('/ping', function (req, res) {
+  res.end('pong')
+})
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
